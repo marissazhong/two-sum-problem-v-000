1dbdde88803a9ee105a6bb1ffefca94cb1bb4ec9
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 15:10:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function binarySearchTwoSum(array, sum) {
   let results = [];
   let new_array = [];
   for (var i = 0; i < sorted_array.length; i++) {
-    new_array = new_array.concat(sorted_array.slice(0,i),sorted_array.slice(i+1,sorted_array.length));
+    let new_array = sorted_array.slice(0,i).push(sorted_array.slice(i+1,sorted_array.length));
     console.log(new_array, sorted_array[i]);
     if (binaryMatch(new_array, sum-sorted_array[i])){
       results.push([sorted_array[i],sum-sorted_array[i]]);
